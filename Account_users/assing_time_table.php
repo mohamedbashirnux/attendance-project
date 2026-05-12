6f8b40d0497c828c7e3d9f3afe4293c688757cc1
Mohamed Bashir <mohamedbashirnux@users.noreply.github.com>
2026-05-12 20:00:13 +0300

    improved

--- Account_users/assing_time_table.php
+++ Account_users/assing_time_table.php
@@ -143,7 +143,7 @@ try {
                 <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
             <div class="toast-body">
-                Timetable entry added successfully!
+                Timetable entry added successfull!
             </div>
         </div>
         <div id="deleteConfirmToast" class="toast bg-warning text-white" role="alert" aria-live="assertive" aria-atomic="true">
