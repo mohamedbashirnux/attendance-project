85c9c7100871bdd2d7124c21d5ca2a66d274ebb6
Mohamed Bashir <mohamedbashirnux@users.noreply.github.com>
2026-05-15 20:57:41 +0300

    just i fix it the erro

--- Account_users/department_simple.php
+++ Account_users/department_simple.php
@@ -7,7 +7,7 @@ $faculty_id = $sessionInfo['faculty_id'];
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Department - Simple </title>
+    <title>Department - Simple Version</title>
     <link rel="stylesheet" href="../assets/vendor/css/core.css" />
 </head>
 <body>
